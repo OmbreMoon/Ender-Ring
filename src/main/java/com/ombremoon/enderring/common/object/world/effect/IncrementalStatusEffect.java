3114105e1222399006857010bb536410a73ec514
DuckXYZMods <itsduckxyz@outlook.com>
2024-06-28 20:53:16 +0100

    Bug fix

--- src/main/java/com/ombremoon/enderring/common/object/world/effect/IncrementalStatusEffect.java
+++ src/main/java/com/ombremoon/enderring/common/object/world/effect/IncrementalStatusEffect.java
@@ -6,7 +6,7 @@ import net.minecraft.world.entity.LivingEntity;
 
 public class IncrementalStatusEffect extends StatusEffect {
     public IncrementalStatusEffect(MobEffectCategory pCategory, int pColor) {
-        super(pCategory, pColor, null, null);
+        super(pCategory, pColor, null, null, null);
     }
 
     public void applyStatusTick(LivingEntity pLivingEntity, ScaledWeapon weapon) {
